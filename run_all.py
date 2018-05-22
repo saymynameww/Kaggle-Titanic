8e84bf56f280313ad231c95d1f23d2daacc7d6f9
saymynameww <wangxk0624@163.com>
2018-05-22 22:20:01 +0800

    052203

---
+++ run_all.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 22 16:40:08 2018
+
+@author: wxk06
+"""
+
+import os
+
+cmd = "python ./preprocess.py"
+os.system(cmd)
+
+cmd = "python ./train.py"
+os.system(cmd)
---
+++ train.py
@@ -0,0 +1,193 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 22 15:10:22 2018
+
+@author: wxk06
+"""
+
+import pandas as pd
+import numpy as np
+import xgboost as xgb
+import pickle
+#%matplotlib inline
+
+import warnings
+warnings.filterwarnings('ignore')
+
+from sklearn.ensemble import (RandomForestClassifier, AdaBoostClassifier, 
+                              GradientBoostingClassifier, ExtraTreesClassifier)
+from sklearn.svm import SVC
+from sklearn.cross_validation import KFold
+
+train = pickle.load(open("data/train_preprocessed.pkl","rb"))
+test = pickle.load(open("data/test_preprocessed.pkl","rb"))
+test_for_ID = pd.read_csv('Data/test.csv')
+PassengerId = test_for_ID['PassengerId']
+# Some useful parameters which will come in handy later on
+ntrain = train.shape[0]
+ntest = test.shape[0]
+SEED = 0 # for reproducibility
+NFOLDS = 5 # set folds for out-of-fold prediction
+kf = KFold(ntrain, n_folds= NFOLDS, random_state=SEED)
+
+# Class to extend the Sklearn classifier
+class SklearnHelper(object):
+    def __init__(self, clf, seed=0, params=None):
+        params['random_state'] = seed
+        self.clf = clf(**params)
+
+    def train(self, x_train, y_train):
+        self.clf.fit(x_train, y_train)
+
+    def predict(self, x):
+        return self.clf.predict(x)
+    
+    def fit(self,x,y):
+        return self.clf.fit(x,y)
+    
+    def feature_importances(self,x,y):
+        print(self.clf.fit(x,y).feature_importances_)
+        
+def get_oof(clf, x_train, y_train, x_test):
+    oof_train = np.zeros((ntrain,))
+    oof_test = np.zeros((ntest,))
+    oof_test_skf = np.empty((NFOLDS, ntest))
+
+    for i, (train_index, test_index) in enumerate(kf):
+        x_tr = x_train[train_index]
+        y_tr = y_train[train_index]
+        x_te = x_train[test_index]
+
+        clf.train(x_tr, y_tr)
+
+        oof_train[test_index] = clf.predict(x_te)
+        oof_test_skf[i, :] = clf.predict(x_test)
+
+    oof_test[:] = oof_test_skf.mean(axis=0)
+    return oof_train.reshape(-1, 1), oof_test.reshape(-1, 1)
+
+# Put in our parameters for said classifiers
+# Random Forest parameters
+rf_params = {
+    'n_jobs': -1,
+    'n_estimators': 500,
+     'warm_start': True, 
+     #'max_features': 0.2,
+    'max_depth': 6,
+    'min_samples_leaf': 2,
+    'max_features' : 'sqrt',
+    'verbose': 0
+}
+
+# Extra Trees Parameters
+et_params = {
+    'n_jobs': -1,
+    'n_estimators':500,
+    #'max_features': 0.5,
+    'max_depth': 8,
+    'min_samples_leaf': 2,
+    'verbose': 0
+}
+
+# AdaBoost parameters
+ada_params = {
+    'n_estimators': 500,
+    'learning_rate' : 0.75
+}
+
+# Gradient Boosting parameters
+gb_params = {
+    'n_estimators': 500,
+     #'max_features': 0.2,
+    'max_depth': 5,
+    'min_samples_leaf': 2,
+    'verbose': 0
+}
+
+# Support Vector Classifier parameters 
+svc_params = {
+    'kernel' : 'linear',
+    'C' : 0.025
+    }
+
+# Create 5 objects that represent our 4 models
+rf = SklearnHelper(clf=RandomForestClassifier, seed=SEED, params=rf_params)
+et = SklearnHelper(clf=ExtraTreesClassifier, seed=SEED, params=et_params)
+ada = SklearnHelper(clf=AdaBoostClassifier, seed=SEED, params=ada_params)
+gb = SklearnHelper(clf=GradientBoostingClassifier, seed=SEED, params=gb_params)
+svc = SklearnHelper(clf=SVC, seed=SEED, params=svc_params)
+
+# Create Numpy arrays of train, test and target ( Survived) dataframes to feed into our models
+y_train = train['Survived'].ravel()
+train = train.drop(['Survived'], axis=1)
+x_train = train.values # Creates an array of the train data
+x_test = test.values # Creats an array of the test data
+
+# Create our OOF train and test predictions. These base results will be used as new features
+et_oof_train, et_oof_test = get_oof(et, x_train, y_train, x_test) # Extra Trees
+rf_oof_train, rf_oof_test = get_oof(rf,x_train, y_train, x_test) # Random Forest
+ada_oof_train, ada_oof_test = get_oof(ada, x_train, y_train, x_test) # AdaBoost 
+gb_oof_train, gb_oof_test = get_oof(gb,x_train, y_train, x_test) # Gradient Boost
+svc_oof_train, svc_oof_test = get_oof(svc,x_train, y_train, x_test) # Support Vector Classifier
+
+print("Training is complete")
+
+
+rf_feature = rf.feature_importances(x_train,y_train)
+et_feature = et.feature_importances(x_train, y_train)
+ada_feature = ada.feature_importances(x_train, y_train)
+gb_feature = gb.feature_importances(x_train,y_train)
+
+rf_features = [0.10474135,  0.21837029,  0.04432652,  0.02249159,  0.05432591,  0.02854371
+  ,0.07570305,  0.01088129 , 0.24247496,  0.13685733 , 0.06128402]
+et_features = [ 0.12165657,  0.37098307  ,0.03129623 , 0.01591611 , 0.05525811 , 0.028157
+  ,0.04589793 , 0.02030357 , 0.17289562 , 0.04853517,  0.08910063]
+ada_features = [0.028 ,   0.008  ,      0.012   ,     0.05866667,   0.032 ,       0.008
+  ,0.04666667 ,  0.     ,      0.05733333,   0.73866667,   0.01066667]
+gb_features = [ 0.06796144 , 0.03889349 , 0.07237845 , 0.02628645 , 0.11194395,  0.04778854
+  ,0.05965792 , 0.02774745,  0.07462718,  0.4593142 ,  0.01340093]
+
+cols = train.columns.values
+# Create a dataframe with features
+feature_dataframe = pd.DataFrame( {'features': cols,
+     'Random Forest feature importances': rf_features,
+     'Extra Trees  feature importances': et_features,
+      'AdaBoost feature importances': ada_features,
+    'Gradient Boost feature importances': gb_features
+    })
+
+# Create the new column containing the average of values
+
+feature_dataframe['mean'] = feature_dataframe.mean(axis= 1) # axis = 1 computes the mean row-wise
+
+base_predictions_train = pd.DataFrame( {'RandomForest': rf_oof_train.ravel(),
+     'ExtraTrees': et_oof_train.ravel(),
+     'AdaBoost': ada_oof_train.ravel(),
+      'GradientBoost': gb_oof_train.ravel()
+    })
+
+x_train = np.concatenate(( et_oof_train, rf_oof_train, ada_oof_train, gb_oof_train, svc_oof_train), axis=1)
+x_test = np.concatenate(( et_oof_test, rf_oof_test, ada_oof_test, gb_oof_test, svc_oof_test), axis=1)
+
+gbm = xgb.XGBClassifier(
+    #learning_rate = 0.02,
+ n_estimators= 2000,
+ max_depth= 4,
+ min_child_weight= 2,
+ #gamma=1,
+ gamma=0.9,                        
+ subsample=0.8,
+ colsample_bytree=0.8,
+ objective= 'binary:logistic',
+ nthread= -1,
+ scale_pos_weight=1).fit(x_train, y_train)
+predictions = gbm.predict(x_test)
+#predictions = gbm.predict(x_test)
+
+#base_score = np.sum((np.tile(y_train,(4,1)).T == base_predictions_train),axis = 0)/len(predictions)
+#score = sum(y_train == predictions)/len(predictions)
+
+# Generate Submission File 
+StackingSubmission = pd.DataFrame({ 'PassengerId': PassengerId,
+                            'Survived': predictions })
+StackingSubmission.to_csv("StackingSubmission.csv", index=False)
